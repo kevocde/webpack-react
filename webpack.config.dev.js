95b09200f6b8a7fb2e4170f9fc41c61b0e7f7e66
Kevin Guzmán <kevocde@gmail.com>
2021-10-13 00:07:01 -0500

    Añadidos archivos de configuracion segun ambientes

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimazerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@styles': path.resolve(__dirname, 'src/styles/')
-    }
+    extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
@@ -52,14 +44,10 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
-    }),
-    new CleanWebpackPlugin()
+    })
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimazerPlugin(),
-      new TerserPlugin()
-    ]
+  devServer: {
+    compress: true,
+    port: 4200
   }
 }
